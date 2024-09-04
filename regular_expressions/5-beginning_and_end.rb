082b54ab4f93ae27548d44e5e92b8739f178e195
maajabualleluiatito <m.tito@alustudent.com>
2024-09-04 18:08:01 +0200

    start and end

---
+++ regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: ./regex_match.rb <string>"
+  exit
+end
+
+# Retrieve the argument
+input_string = ARGV[0]
+
+# Match the regular expression against the input string
+matches = input_string.scan(/School/)
+
+# Concatenate the matches
+concatenated_string = matches.join
+
+# Print the concatenated string and its length
+#puts "#{concatenated_string} (#{concatenated_string.length} chars long)"
+puts ARGV[0].scan(/h.n/).join
