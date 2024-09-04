8337a808587bf374dd971cb04ec713dd58333058
maajabualleluiatito <m.tito@alustudent.com>
2024-09-04 17:58:12 +0200

    second repetition

---
+++ regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: ./regex_match.rb <string>"
+  exit
+end
+
+# Retrieve the argument
+input_string = ARGV[0]
+
+# Match the regular expression against the input string
+match = input_string.match(/School/)
+
+# Print the matched string
+puts ARGV[0].scan(/hb?tn/).join
