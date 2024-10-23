954d1db358da0a101da0d7880882f16718313310
maajabualleluiatito <m.tito@alustudent.com>
2024-10-23 11:52:19 +0200

    asdvas

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
-"""Module documentation"""
+"""fetches the title of all hot posts for a given subreddit recursively"""
+
 import requests
 
-headers = {'User-Agent': 'MyAPI/0.0.1'}
 
+def recurse(subreddit, hot_list=[], after=""):
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-def recurse(subreddit, after="", hot_list=[], page_count=0):
-
-    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
-
-    parameters = {'limit': 100, 'after': after}
-    response = requests.get(subreddit_url, headers=headers, params=parameters)
-
-    if response.status_code == 200:
-        json_data = response.json()
-
-        for child in json_data.get('data').get('children'):
-            title = child.get('data').get('title')
-            hot_list.append(title)
-
-        after = json_data.get('data').get('after')
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    PARAMS = {"after": after, "limit": 100}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, params=PARAMS,
+                                allow_redirects=False)
+        after = RESPONSE.json().get("data").get("after")
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [hot_list.append(post.get('data').get('title')) for post in HOT_POSTS]
         if after is not None:
-
-            page_count += 1
-            return recurse(subreddit, after=after,
-                           hot_list=hot_list, page_count=page_count)
-        else:
-            return hot_list
-
-    else:
+            return recurse(subreddit, hot_list, after)
+        return hot_list
+    except Exception:
         return None
-
-
-if __name__ == '__main__':
-    print(recurse("zerowastecz"))
--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
-"""Module documentation"""
-import json
+"""fetches the title of all hot posts for a given subreddit recursively"""
+
 import requests
 
 
-def count_words(subreddit, word_list, after='', hot_list=None):
-    if hot_list is None:
-        hot_list = [0] * len(word_list)
+def count_words(subreddit, word_list=[], hot_list=[], after=""):
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url, params={'after': after},
-                           allow_redirects=False,
-                           headers={'User-Agent': 'My User Agent 1.0'})
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    PARAMS = {"after": after, "limit": 100}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, params=PARAMS,
+                                allow_redirects=False)
+        after = RESPONSE.json().get("data").get("after")
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [hot_list.append(post.get('data').get('title')) for post in HOT_POSTS]
+        if after is not None:
+            return count_words(subreddit, word_list, hot_list, after)
 
-    if request.status_code == 200:
-        data = request.json()
-
-        for topic in data['data']['children']:
-            title_words = topic['data']['title'].split()
-            for word in title_words:
-                for i, keyword in enumerate(word_list):
-                    if keyword.lower() == word.lower():
-                        hot_list[i] += 1
-
-        after = data['data']['after']
-        if after is None:
-            word_counts = {}
-            for i, word in enumerate(word_list):
-                if word not in word_counts:
-                    word_counts[word] = hot_list[i]
+        new_dict = {}
+        word_list = set([wrd.lower() for wrd in word_list])
+        for title in hot_list:
+            for word in title.split():
+                if word.lower() in new_dict:
+                    new_dict[word.lower()] += 1
                 else:
-                    word_counts[word] += hot_list[i]
+                    new_dict.update({word.lower(): 1})
 
-            sorted_counts = sorted(
-                word_counts.items(),
-                key=lambda x: (-x[1], x[0].lower())
-            )
-
-            for word, count in sorted_counts:
-                if count > 0:
-                    print("{}: {}".format(word.lower(), count))
-        else:
-            count_words(subreddit, word_list, after, hot_list)
+        sorted_dict = sorted(new_dict.items(), key=lambda x: (-x[1], x[0]))
+        for key, value in sorted_dict:
+            if (key in word_list) and (value > 0):
+                print("{}: {}".format(key, value))
+    except Exception:
+        return None
